2d487701499c07e31f600bb1d97bcb4477541d8b
zh <1749198910@qq.com>
2023-01-05 16:50:52 +0800

    修改

--- src/main/java/cloud/zhenghang/wechat/mapper/CountdownMapper.java
+++ src/main/java/cloud/zhenghang/wechat/mapper/CountdownMapper.java
@@ -2,6 +2,7 @@ package cloud.zhenghang.wechat.mapper;
 
 import cloud.zhenghang.wechat.domain.Countdown;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
 * @author gcddd
@@ -9,6 +10,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @createDate 2022-11-18 16:07:33
 * @Entity cloud.zhenghang.wechat.domain.Countdown
 */
+@Mapper
 public interface CountdownMapper extends BaseMapper<Countdown> {
 
 }
